f4dfd25417cf44b560121f4970ac1a328cb356f6
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-05-16 09:54:56 +0530

    Add: selection practice problems with if and else

--- SequencePracticeProblem/AddRandomDice.js
+++
@@ -1,8 +0,0 @@
-//Q.3
-var randomDice1 = Math.floor(Math.random() *6)+1;
-var randomDice2 = Math.floor(Math.random() *6)+1;
-
-var sum = randomDice1+randomDice2;
-console.log("dice 1:"+randomDice1);
-console.log("dice 2:"+randomDice2);
-console.log("sum :" + sum);
--- SequencePracticeProblem/toGetSingleDigit.js
+++
@@ -1,3 +0,0 @@
-//Q.1
-var randomNumber=Math.floor(Math.random() *10);
-console.log("Random single-digit number:"+randomNumber);
